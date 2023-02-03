ff69751783261bf04d9e2300d41706a6b54297bf
Ludovic Hauchemaille <ludovic.hauchemaille@gmail.com>
2023-02-03 23:42:24 +0100

    feat: Send request

--- README.md
+++ README.md
@@ -1,96 +1,95 @@
-# Obsidian Sample Plugin
+# Obsidian request sender Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+This is a request sender plugin for Obsidian (https://obsidian.md).
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+This project allows to add buttons on Obsidian notes that will send REST requests and display the request's response.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
-
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Changes the default font color to red using `styles.css`.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
-
-## First time developing plugins?
-
-Quick starting guide for new plugin devs:
-
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
-
-## Releasing new releases
-
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
-
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
-
-## Adding your plugin to the community plugin list
-
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+![Request example](./readme_request_example.png)
 
 ## How to use
 
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
+Add block code with the request parameters:
 
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
+``` request-sender
+title: <Button title>
+type: GET | POST | PUT | DELETE
+url: <Requst URL>
+headers: <[Optional] Request headers>
+body: <[Optional] Request body>
+formData: <[Optional] Request form data>
 ```
 
-If you have multiple URLs, you can also do:
+Note: parameters must respect YAML format.
 
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
+## Examples
+
+
+Simple GET request:
+
+
+``` request-sender
+title: Example GET
+type: GET
+url: https://petstore.swagger.io/v2/pet/findByStatus?status=available
 ```
 
-## API Documentation
 
-See https://github.com/obsidianmd/obsidian-api
+POST request with body:
+
+
+``` request-sender
+title: Example POST body
+type: POST
+url: https://petstore.swagger.io/v2/pet
+headers:
+  Content-type: application/json
+body: >
+  {
+    "id": 0,
+    "category": {
+      "id": 0,
+      "name": "category-example"
+    },
+    "name": "doggie",
+    "photoUrls": [ "https://example.org/some_picture_1.png", "https://example.org/some_picture_2.png" ],
+    "tags": [
+      {
+        "id": 0,
+        "name": "example"
+      }
+    ],
+    "status": "available"
+  }
+```
+
+
+PUT request with XML data:
+
+
+``` request-sender
+title: Example PUT with XML data
+type: PUT
+url: https://petstore.swagger.io/v2/pet/
+headers:
+  Content-type: application/xml
+body: > 
+  <?xml version="1.0" encoding="UTF-8"?>
+  <Pet>
+  	<id>0</id>
+  	<Category>
+  		<id>0</id>
+  		<name>string</name>
+  	</Category>
+  	<name>doggie</name>
+  	<photoUrls>
+  		<photoUrl>string</photoUrl>
+  	</photoUrls>
+  	<tags>
+  		<Tag>
+  			<id>0</id>
+  			<name>string</name>
+  		</Tag>
+  	</tags>
+  	<status>available</status>
+  </Pet>
+```
--- main.ts
+++ main.ts
@@ -1,137 +1,130 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { ButtonComponent, MarkdownPostProcessorContext, parseYaml, Plugin, setIcon } from 'obsidian';
+import axios, { AxiosHeaders, HttpStatusCode } from 'axios';
 
-// Remember to rename these classes and interfaces!
+type Status = 'success' | 'error';
+type RequestType = 'POST' | 'GET' | 'DELETE' | 'PUT';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface RequestResult {
+  status: HttpStatusCode;
+  data: any;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface RequestData {
+  title?: string;
+  type: RequestType;
+  url: string;
+  headers?: Headers;
+  formData?: FormData[];
+  body?: string;
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  private resultContainer?: HTMLElement;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  async onload() {
+    this.registerMarkdownCodeBlockProcessor("request-sender", (source, el, ctx) => { this.codeBlockHandler(source, el, ctx) });
+  }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+  onunload() {
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  private codeBlockHandler(source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext): void {
+    try {
+      const mainContainer = el.createEl('div', { cls: 'main-container' });
+      const buttonsContainer = mainContainer.createEl('div', { cls: 'flex-container' });
+      const subContainer = mainContainer.createEl('div');
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+      const requestData: RequestData = parseYaml(source);
+      if (!requestData.url) throw new Error('URL must be provided.');
+      if (!requestData.type) throw new Error(`Request type must be provided. ('POST' | 'GET' | 'DELETE' | 'PUT')`);
+      if (!requestData) throw new Error('Request data does not have all required parameters.')
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+      const buttonSend = new ButtonComponent(buttonsContainer);
+      buttonSend.setClass('button');
+      buttonSend.setButtonText(requestData.title ? requestData.title : 'Send request!');
+      buttonSend.onClick(async () => {
+        try {
+          if (this.resultContainer) this.resultContainer.remove();
+          const response: RequestResult = await this.sendRequest(requestData);
+          this.createResponseElement(
+            subContainer, 
+            `Status: ${response.status}`, 
+            JSON.stringify(response.data, undefined, 2), 
+            this.getStatusForResponse(response.status));
+        } catch (error) {
+          this.createResponseElement(subContainer, 'Error', error, 'error');
+        }
+      });
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+      const buttonClear = new ButtonComponent(buttonsContainer);
+      buttonClear.setClass('button');
+      buttonClear.setButtonText('Clear');
+      buttonClear.onClick(async () => {
+        if (this.resultContainer) this.resultContainer.remove();
+      });
+    } catch (error) {
+      this.createResponseElement(el, 'Error', error, 'error');
+    }
+  }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+  private getStatusForResponse(statusCode: HttpStatusCode): Status | undefined {
+    return statusCode >= 200 && statusCode < 300 ? 'success' : statusCode >= 300 ? 'error' : undefined;
+  }
 
-	onunload() {
+  private createResponseElement(el: HTMLElement, title: string, content: string, status?: Status): void {
+    this.resultContainer = el.createEl('div', { cls: 'container' }); 
 
-	}
+    const titleDiv = this.resultContainer.createEl('div', { cls: 'flex-container content status' });
+    if (status) setIcon(titleDiv.createEl('span', { cls: 'icon' }), status === 'error' ? 'alert-circle' : 'check-circle-2');
+    titleDiv.createEl('span', { text: title });
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    const contentDiv = this.resultContainer.createEl('pre', { text: content, cls: 'content' });
+    titleDiv.toggleClass('status-success', status === 'success');
+    titleDiv.toggleClass('status-error', status === 'error');
+    
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+  private async sendRequest(requestData: RequestData): Promise<RequestResult> {
+    let result: RequestResult = { status: HttpStatusCode.Created, data: '' };
+    let headers: AxiosHeaders | undefined;
+    if (requestData.headers) {
+      headers = new AxiosHeaders();
+      Object.entries(requestData.headers).forEach((header: any) => { if (headers) headers.set(header[0], header[1]) })
+    }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+    switch (requestData.type) {
+      case 'POST': {
+        let formData = new FormData();
+        if (requestData.formData) Object.entries(requestData.formData).forEach((data: any) => formData.append(data[0], data[1]));
+        
+        const response = await axios.post(requestData.url, requestData.body ? requestData.body : formData, { headers });
+        result.status = response.status;
+        result.data = response.data;
+        break;
+      }
+      case 'GET': {
+        const response = await axios.get(requestData.url, { headers });
+        result.status = response.status;
+        result.data = response.data;
+        break;
+      }
+      case 'DELETE': {
+        const response = await axios.delete(requestData.url, { headers });
+        result.status = response.status;
+        result.data = response.data;
+        break;
+      }
+      case 'PUT': {
+        const response = await axios.put(requestData.url, requestData.body ? requestData.body : requestData.data, { headers });
+        result.status = response.status;
+        result.data = response.data;
+        break;
+      }
+      default:
+        throw new Error(`${requestData.type} method not supported.`);
+    }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    return result;
+  }
 }
